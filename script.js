bfd052a29ebc906924ff1f1920fcf31428a262cd
Ismoil57 <mismoil7757@GMAIL.COM>
2022-10-12 01:06:26 +0500

    lesson-2

---
+++ script.js
@@ -0,0 +1,25 @@
+let x= (Math.pow(4, 2)+ Math.sqrt(9))/3
+console.log(x)
+// --------
+
+// 1 sm-m
+{
+    let sm = prompt("sm kiriting");
+    console.log(sm/100 + " m")
+}
+// 
+
+// 2 kg-t
+
+console.log(250 / 1000 + " t")
+
+// 
+
+// 3 a>b
+
+console.log(10 < 12)
+console.log(15 < 120)
+
+// 
+
+// 
